c283e2ab900c192809282289e9dfe57491fcd657
Sultan-Alabi <sulttyboy@gmail.com>
2022-09-08 04:55:12 -0700

    put file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - Beginning
+ *
+ * Description: puts command
+ *
+ * return: always 0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 
 }
