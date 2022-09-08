c82fab4d40ca55f05d6425f4abfa589e187d89ea
Sultan-Alabi <sulttyboy@gmail.com>
2022-09-08 08:05:51 -0700

    6-size file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,20 +6,14 @@
  * Return: Always 0 (Success)
  */
 
-int main()
-
+int main(void)
 {
-	int inttype;
-		char chartype;
-		int inttype;
-		long int longinttype;
-		long long int longlonginttype;
-		float floattype;
-	printf("Size of a char: %zu byte(s)\n", sizeof(chartype));
-	printf("Size of a int: %zu byte(s)\n", sizeof(inttype));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(longint));
-	printf("Size of a long long int %zu byte(s)\n", sizeof(longlonginttype));
-	printf("Size of a floattype %zu byte(s)\n", sizeof(floattype));
+	printf("Size of a char: %zu bytes\n", sizeof(char));
+	printf("Size of an int: %zu bytes\n", sizeof(int));
+	printf("Size of long int: %zu bytes\n", sizeof(long int));
+	printf("Size of long long int: %zu bytes\n", sizeof(long long int));
+	printf("Size of float: %zu bytes\n", sizeof(float));
 
 	return (0);
 }
+
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,1 @@
-6-size
+size
