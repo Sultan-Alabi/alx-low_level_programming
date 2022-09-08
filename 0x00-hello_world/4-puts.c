4b9557964c5cb1beab8659a5b58f76f5a026aaf4
Sultan-Alabi <sulttyboy@gmail.com>
2022-09-08 04:41:49 -0700

    put file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-main int()
+int main(void)
 {
-	int puts(const char* Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle")
 
 	return 0;
 
