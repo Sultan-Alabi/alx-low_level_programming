bec3ca91eea12416bf0b20f419153a822fd478ab
Sultan-Alabi <sulttyboy@gmail.com>
2022-09-09 15:47:21 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++_
+	{
+		putchar (alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
