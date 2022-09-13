ffadb029a14b7f015b98131d4ab38548b16c43a5
Sultan-Alabi <sulttyboy@gmail.com>
2022-09-13 15:47:18 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * int _abs(int) - Starting point
+ *
+ * int: input paramter
+ *
+ * Return: 0, 1
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n* -1);
+	}
+	else
+	{
+		return (n);
+	}
+	_putchar('\n');
+}
