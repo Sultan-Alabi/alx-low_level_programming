ca9f7b8e4f96e5c72625a920483e4d21474bd275
Sultan-Alabi <sulttyboy@gmail.com>
2022-09-13 14:55:35 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - Start point
+ *
+ * @c: input parameter
+ *
+ * Return: 1 (Succerss) 0 (Not)
+ */
+
+int _isalpha(void)
+{
+	if ((c > 96 && c < 123) || (c > 64 && c <91))
+	{
+		return (1)
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
