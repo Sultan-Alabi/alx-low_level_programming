06a608f15b685438c0a2018037c0a0b54a8250e1
Sultan-Alabi <sulttyboy@gmail.com>
2022-09-13 17:06:35 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,29 +8,10 @@
 
 void print_to_98(int n)
 {
-	if (n > 98)
+	int print[100];
+	while (print = n; print < 98; print++)
 	{
-		while (n >= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(",");
-			}
-			n--;
-		}
+		_putchar(print);
 	}
-	else
-	{
-		while (n <= 98)
-		{
-			printf("%d", n);
-				if (n != 98)
-				{
-					printf(",");
-				}
-				n++;
-		}
-	}
-	printf("\n");
+	return (0);
 }
