3080ec1dd012da598866e849d57a4f8e8884ef51
Sultan-Alabi <sulttyboy@gmail.com>
2022-09-15 10:46:10 -0700

    _isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main"
+
+/**
+ * _isdigit - Starting point
+ *
+ * @c: input parameter
+ *
+ * Return: 1 if positve and 0 otherwise
+ */
+
+int _isdigit(int c);
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
