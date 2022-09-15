afdcbc43e46a29215225a25e3be60a40b8187aa9
Sultan-Alabi <sulttyboy@gmail.com>
2022-09-15 15:23:27 -0700

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Start point
+ * i: input parameter
+ * Return: Answer
+ */
+
+void print_numbers(void)
+{
+	int j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
