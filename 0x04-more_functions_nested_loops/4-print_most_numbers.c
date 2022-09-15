c02fdb3785c682c19eaff8b6e26c656ae469111a
Sultan-Alabi <sulttyboy@gmail.com>
2022-09-15 15:41:05 -0700

    print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Starting 
+ * @j: input
+ * Return: Answer
+ */
+
+void print_most_numbers(void)
+{
+	int j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		if (j == 2 && j == 4)
+		{
+			continue;
+		}
+		else
+		{
+		_putchar(j + '0');
+		}
+	}
+	_putchar('\n');
+}
