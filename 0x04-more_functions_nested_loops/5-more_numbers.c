790ef654c89081399573b1a0a9ec624af648c975
Sultan-Alabi <sulttyboy@gmail.com>
2022-09-15 16:31:20 -0700

    more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * more_numbers
+ * more_numbers - Starting point
  * Return: answer
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i;
+	int j;
 
 	for (j = 0; j < 10; j++)
 	{
