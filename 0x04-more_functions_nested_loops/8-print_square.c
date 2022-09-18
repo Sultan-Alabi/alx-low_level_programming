1b4dde9508463deca966a4ab775a7ba106002968
Sultan-Alabi <sulttyboy@gmail.com>
2022-09-18 05:40:11 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - Starting point
+ *
+ * @size: draws square
+ *
+ * Return: answer
+ */
+
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	if (size > 0)
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
