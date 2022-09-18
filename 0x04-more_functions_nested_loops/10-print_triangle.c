73b3e81e9389c334cae1a859a029ec980d32bacc
Sultan-Alabi <sulttyboy@gmail.com>
2022-09-18 06:46:39 -0700

    print_triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,16 +1,31 @@
 #include "main.h"
 
 /**
- * main - Starting point
+ * print_triangle - Starting point
  *
  * Return: Answer
  */
-
-int main(void)
+void print_triangle(int size)
 {
-	print_triangle(2);
-	print_triangle(10);
-	print_triangle(1);
-	print_triangle(0);
-	return (0);
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i= 0; i <= (size - 1); i++)
+		{
+			for (j = 0; j <= (size - 1) - i; j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k < i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
