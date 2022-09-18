52d314b351655bf5f29214b39b62fd9b4027f670
Sultan-Alabi <sulttyboy@gmail.com>
2022-09-18 05:26:59 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,20 +11,15 @@ void print_diagonal(int n)
 	int i;
 	int c;
 
-	if (n <= 0)
+	for (i = 0; i < n; i++)
 	{
+		for (c = 0; c < i; c++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		if (i < (n -1))
 		_putchar('\n');
 	}
-	else
-	{
-		for (i = 1; i <= n; i++)
-		{
-			for (c = 1; c < i; c++)
-			{
-				_putchar('_');
-			}
-			_putchar('\\');
-			_putchar('\n');
-		}
-	}
+	_putchar('\n');
 }
