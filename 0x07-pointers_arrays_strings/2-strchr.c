b88515e5f51d3df9abfb4a80db90203f58fa0ec1
Sultan-Alabi <sulttyboy@gmail.com>
2022-09-27 02:33:51 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - Starting point
+ * @s: Input
+ * @c: Input
+ * Return: Null Character
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	for (; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) == c)
+		{ 
+			return (s + i);
+		}
+	}
+	if (*(s + i) == c)
+	{
+		return (s + i);
+	}
+	return ('\0');
+}
